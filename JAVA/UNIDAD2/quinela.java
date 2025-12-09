e97550ddf90836fb82e2076c102cdd21726c0400
unknown <bamcuent312@gmail.com>
2025-12-09 20:49:00 +0100

    quinela java

---
+++ JAVA/UNIDAD2/quinela.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class QuinielaTresColumnas {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        String[] signos = {"1", "X", "2"};
+
+        for (int i = 1; i <= 14; i++) {
+            System.out.print(signos[rand.nextInt(3)] + " ");
+            System.out.print(signos[rand.nextInt(3)] + " ");
+            System.out.println(signos[rand.nextInt(3)]);
+        }
+
+        // Pleno al 15
+        System.out.print(signos[rand.nextInt(3)] + " ");
+        System.out.print(signos[rand.nextInt(3)] + " ");
+        System.out.println(signos[rand.nextInt(3)]);
+    }
+}
